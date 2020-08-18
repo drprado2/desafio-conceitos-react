aca92b89fdb240794988bb54f162533856731cc9
Adriano Oliveira <drprado2@gmail.com>
2020-08-18 20:17:41 -0300

    Implementações features do desafio

--- src/App.js
+++ src/App.js
@@ -1,29 +1,67 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "Título do seu repositório",
+    url: "http://minhaurl.example",
+    techs: "JS, C#"
+  })
+
+  useEffect(() => {
+    api.get('repositories').then(r => setRepositories(r.data));
+  }, []);
+
+  function handleFormChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value
+    })
+  }
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', formData);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(r => r.id !== id) || []);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map(r =>
+          <li key={r.id}>
+            {r.title}
+            <button onClick={() => handleRemoveRepository(r.id)}>
+              Remover
+            </button>
+          </li>)}
       </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <div>
+          <div>
+            <label htmlFor="title">Título</label>
+            <br/>
+            <input type="text" name="title" id="title" value={formData.title} onChange={handleFormChange}/>
+          </div>
+          <div>
+            <label htmlFor="url">Url</label>
+            <br/>
+            <input type="text" name="url" id="url" value={formData.url} onChange={handleFormChange}/>
+          </div>
+          <div>
+            <label htmlFor="techs">Tecnologias</label>
+            <br/>
+            <input type="text" name="techs" id="techs" value={formData.techs} onChange={handleFormChange}/>
+          </div>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
